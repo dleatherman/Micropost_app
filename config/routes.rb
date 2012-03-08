52f18930d7a7950e933a0457577e67a3022d7196
Dan Leatherman <dleatherman@gmail.com>
2012-03-08 12:29:20 -0500

    modify root and remove default index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ MicropostApp::Application.routes.draw do
 
   get "static_pages/help"
 
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
