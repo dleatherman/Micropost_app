7e7d068a9c9c553396a2b31bfd1a890c764a62d3
Dan Leatherman <dleatherman@gmail.com>
2012-03-08 21:50:14 -0500

    add contact page and test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MicropostApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 
   root :to => 'static_pages#home'
 
